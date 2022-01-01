db34970f6178ec8a5c7855d6d4a4984670a8e062
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2022-01-01 13:02:13 -0500

    Update gs.py

--- gs.py
+++ gs.py
@@ -2,8 +2,9 @@ import gspread
 import pandas as pd
 
 gc = gspread.service_account()
-df_sheet = gc.open("#sheet#")
+df_sheet = gc.open("BGR_TATL_flights")
 df_ws = df_sheet.get_worksheet(0)
 df = pd.DataFrame(df_ws.get_all_values())
 df.columns = df.iloc[0,:]
 df = df.iloc[1:,:]
+init_len = len(df)
