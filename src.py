50d0a2d9784b434815db7ceb2a5bcdf3c2fdcc78
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2022-01-01 13:02:32 -0500

    Update src.py

--- src.py
+++ src.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-user = "#user#"
-key = "#key#"
+user = "hloughlin9"
+key = "5fb2d8528088de31addbf004f20248875e3a804d"
 payload = {"airport":"KBGR", "howMany":15}
 url = "https://flightxml.flightaware.com/json/FlightXML2/"
 
