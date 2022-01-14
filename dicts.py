38972d113f06211972d4c67efce54832cdb9b58e
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2022-01-14 14:56:43 -0500

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 cols = ['FA','SS']
 
-ac = pd.read_csv("\\data\\AC.csv", names=cols)
+ac = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\AC.csv", names=cols)
 ac_dict = dict(zip(ac['FA'],ac['SS']))
 
-al = pd.read_csv("\\data\\AL.csv", names=cols)
+al = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\AL.csv", names=cols)
 al_dict = dict(zip(al['FA'],al['SS']))
 
-codes = pd.read_csv("\\data\\ICAOIATA.csv", names=cols)
+codes = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\ICAOIATA.csv", names=cols)
 code_dict = dict(zip(codes['FA'],codes['SS']))
 
-apc = pd.read_csv("\\data\\APC.csv")
+apc = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\APC.csv")
 apc_dict = dict(zip(apc['IATA'],apc['Country']))
