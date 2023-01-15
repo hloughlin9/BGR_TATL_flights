1ce1b3274818079e179c084e04324db99d1ed481
Speedbird45Bravo <henry.loughlin@gmail.com>
2023-01-15 10:20:23 -0500

    Update flight_sheet.py.

--- flight_sheet.py
+++ flight_sheet.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 
 # Loading the spreadsheet.
 def get_sheet():
-    gc = gspread.service_account("##### creds_path.json #####")
+    gc = gspread.service_account("##### credentials_path.json #####")
     df_sheet = gc.open("BGR_TATL_flights")
     df_worksheet = df_sheet.get_worksheet(0)
     df = DataFrame(df_worksheet.get_all_values()[1:])
