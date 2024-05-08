69679c28a31c32821d87d51d57325cc23aa548bf
Henry Loughlin <64449842+hloughlin9@users.noreply.github.com>
2024-05-08 09:13:27 -0400

    Update main.py
    Consistent spacing.

--- main.py
+++ main.py
@@ -43,58 +43,46 @@ pd.options.display.max_columns = 20
 et = tz.gettz("America/New_York")
 dict_cols = ['FA', 'SS']
 
-
 # Below, two CSVs that we import as dictionaries, essentially.
-
 # One maps airline ICAO identifiers (e.g. BAW for British Airways)
 # to airline names.
 ac = pd.read_csv("AC.csv", names=dict_cols)
 ac_dict = dict(zip(ac['FA'], ac['SS']))
-
 
 # Another maps aircraft ICAO identifiers (e.g. B744 for Boeing 747-400)
 # to aircraft names.
 al = pd.read_csv("AL.csv", names=dict_cols)
 al_dict = dict(zip(al['FA'], al['SS']))
 
-
 # Loading the spreadsheet.
 df, df_worksheet = get_sheet()
-
 
 # Get the initial length.
 initial_length = len(df)
 
-
 # Previous flights added 6/27/2022
 prev_flights = set(df['ID'])
-
 
 # Get the current date and time.
 now = dt.now(tz=et).strftime("%Y-%m-%d %H:%M:%S")
 print(f"Flights retrieved from FlightAware AeroAPI query at {now}.\n")
 
-
 # A couple of date transformations to convert the initial dates into the correct ones.
 bgr['Date'] = pd.to_datetime(bgr['Date'])
 bgr['Date'] = bgr['Date'].dt.tz_convert("US/Eastern")
 
-
 # This ensures that our ICAO identifiers all have 4 letters.
 bgr = bgr[(bgr['origin_icao'].str.len() == 4) &
           (bgr['destination_icao'].str.len() == 4)].reset_index(drop=True)
-
 
 # Now that we have our arrival and departures together (for a given pull),
 # we will capture the identifiers.
 idents_a = []
 idents_b = []
 
-
 # This grabs the airline (ICAO).
 for col in bgr['ident']:
     idents_a.append(re.split("(\\d+)", col)[0])
-
 
 # Excluding singular letter-based identifiers without any numeric values,
 # we'll be able to grab most flight numbers here.
@@ -104,7 +92,6 @@ for col in bgr['ident']:
     except IndexError:
         idents_b.append("None")
 
-
 # Converting the airline ICAO codes from the API pull to a dictionary of
 # codes listed as they are in the spreadsheet.
 bgr['airline_sym'] = pd.Series(idents_a)
@@ -112,10 +99,8 @@ bgr['airline'] = bgr['airline_sym'].map(al_dict)
 bgr['flight'] = idents_b
 bgr['type'] = bgr['type'].map(ac_dict)
 
-
 # Drop the null aircraft 1/8/2022
 bgr = bgr[bgr['type'].notna()]
-
 
 # We want to filter out flights that are entirely arriving and departing from the US (starting with "K"), Canada
 # (starting with "C"), Mexico (starting with "M"), Greenland (starting with "BG"), or the Caribbean (starting with "T").
@@ -129,9 +114,7 @@ bgr = bgr[(((bgr['origin_icao'].str[0] != "K") & (bgr['origin_icao'].str[0] != "
               ((bgr['flight'] != "901") | (bgr['airline'] != "N"))
               ))]
 
-
 bgr['Date'] = bgr['Date'].apply(lambda x: dt.strftime(x, "%Y-%m-%d"))
-
 
 # ID serialization.
 bgr['id'] = bgr['Date'].astype(str) + bgr['airline_sym'].astype(str) + bgr['flight'].astype(str)
@@ -139,20 +122,16 @@ bgr['id'] = bgr['id'].str.replace("-", "")
 bgr['id'] = bgr['id'].str[2:]
 bgr['id'] = bgr['id'].str.replace("nan", "")
 
-
 # Replacing None flight numbers with nothing.
 bgr['id'] = bgr['id'].str.replace("None", "")
 bgr['flight'] = bgr['flight'].str.replace("None", "")
 bgr['origin'] = bgr['origin'].str.replace("None", np.isnan)
 bgr['destination'] = bgr['destination'].str.replace("None", np.isnan)
 
-
-
 # If there is no IATA code, go to the ICAO for backup. In honor of LSZL, which has no IATA code,
 # these are unofficially dubbed the Locarno locs.
 bgr['origin'] = np.where(bgr['origin'].isnull(), bgr['origin_icao'], bgr['origin'])
 bgr['destination'] = np.where(bgr['destination'].isnull(), bgr['destination_icao'], bgr['destination'])
-
 
 # An ordered list to use. This will change prior to the upload, but we will need certain fields for subsetting the data.
 ordered = ['Date', 'id', 'airline', 'flight', 'type', 'origin', 'origin_country', 'destination', 'destination_country']
@@ -169,29 +148,23 @@ bgr = bgr[~bgr['id'].isin(prev_flights)]
 # Get the length of the bgr DataFrame.
 bgr_length = len(bgr)
 
-
 # Drop any records with both the origin and destination having the same country OR not being able to determine them.
 bgr = bgr[~((bgr['origin_country'] == "US") & (bgr['destination_country'] == "US")) &
           ((bgr['origin_country'] != "None") & (bgr['destination_country'] != "None"))]
-
 
 # Order the DataFrame by the final columns order.
 bgr = bgr.rename(columns={"id":"ID", "airline":"Airline", "flight":"Flight", "type":"Type",
                           "origin":"Origin", "origin_country":"Origin Country", "destination":"Destination",
                           "destination_country":"Destination Country"})
 
-
 # Drop duplicate code chained 13:01 1/1/2022
 df_final = pd.concat([df, bgr], axis=0).reset_index(drop=True)
-
 
 # Sort values isolated 10:34 2/5/2022
 df_final = df_final.sort_values(by=['Date'])
 
-
 # Get the end length of the DataFrame.
 df_end_length = len(df_final)
-
 
 # Bool length calc rebuilt 6/25/2022
 if initial_length == df_end_length:
@@ -201,11 +174,9 @@ if initial_length == df_end_length:
 else:
     pass
 
-
 # Print flights logic created 5/26/2022, amended 5/28/2022, replaced 6/4/2022
 print(f"{bgr_length} flights added. {df_end_length} flights total\n")
 print(f"Flight(s) added to BGR_TATL_flights:\n{bgr}")
-
 
 # Set the worksheet as the new version.
 set_with_dataframe(df_worksheet, df_final)
